8be2e34249e08e131f81f66c6017bc0e18dd5c96
te14.elias.grafstrom@umea.nti.se <FaLLaffel9812!>
2016-09-09 10:54:35 +0200

    Rast Öhm, går segt

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,12 @@ public class Main{
     public static void main(String[] arg){
         String user1;
         String user2;
+        char[] plan = new char[9];
 
         user1   =   JOptionPane.showInputDialog(null, "Ange namn för spelare 1.");
         user2   =   JOptionPane.showInputDialog(null, "Ange namn för spelare 2.");
 
-        System.out.println(user1 + " "+ user2);
+
     }
 
 }
