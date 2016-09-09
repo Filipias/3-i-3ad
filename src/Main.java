7f6d6a08fc0193dc66b24d3a14783c974568d16f
te14.elias.grafstrom@umea.nti.se <FaLLaffel9812!>
2016-09-09 09:49:03 +0200

    Rast

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import   javax.swing.*;
 
-public class Main {
-    public static void Main(String[]args){
-        System.out.println("YOLO");
+public class Main{
+
+    public static void main(String[] arg){
+        String user1;
+        String user2;
+
+        user1   =   JOptionPane.showInputDialog(null, "Ange namn för spelare 1.");
+        user2   =   JOptionPane.showInputDialog(null, "Ange namn för spelare 2.");
+
+        System.out.println(user1 + " "+ user2);
     }
+
 }
